5d25b904d553d14c6b84ad2a37a9640cddc827ed
claresta1704 <duoce2005@gmail.com>
2024-04-17 08:36:03 +0700

    kombinasi password

--- src/api/components/users/users-validator.js
+++ src/api/components/users/users-validator.js
@@ -5,7 +5,12 @@ module.exports = {
     body: {
       name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
-      password: joi.string().min(6).max(32).required().label('Password'),
+      password: joi.string().min(6).max(32).required().label('Password')
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,32}$/)
+        .label('Password')
+        .messages({
+          'string.pattern.base': 'Password wajib mengandung kombinasi huruf besar, huruf kecil, simbol, dan angka',
+        }),
       passwordConfirm: joi.string().min(6).max(32).required().label('Password_Confirm'),
     },
   },
@@ -20,7 +25,12 @@ module.exports = {
   changePassword: {
     body: {
       oldPassword: joi.string().min(6).max(32).required().label('Old Password'),
-      newPassword: joi.string().min(6).max(32).required().label('New Password'),
+      newPassword: joi.string().min(6).max(32).required().label('New Password')
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,32}$/)
+        .label('New Password')
+        .messages({
+          'string.pattern.base': 'Password wajib mengandung kombinasi huruf besar, huruf kecil, simbol, dan angka',
+        }),
       newPasswordConfirm: joi.string().min(6).max(32).required().label('New Password Confirm'),
     },
   },
